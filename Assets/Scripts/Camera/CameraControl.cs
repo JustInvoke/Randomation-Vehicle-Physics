d57352b98361ee3a36c94503c1ed276f16de33e9
Justin Couch <justin@justinvoke.com>
2020-11-25 09:41:00 -0800

    Replaced new Vector3 with Vector3.up * height in CameraControl

--- Assets/Scripts/Camera/CameraControl.cs
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -102,7 +102,7 @@ namespace RVP
                 lookObj.position = target.position;
                 Vector3 lookDirActual = (lookDir - new Vector3(Mathf.Sin(smoothYRot), 0, Mathf.Cos(smoothYRot)) * Mathf.Abs(smoothYRot) * 0.2f).normalized;
                 Vector3 forwardDir = lookObj.TransformDirection(lookDirActual);
-                Vector3 localOffset = lookObj.TransformPoint(-lookDirActual * distance - lookDirActual * Mathf.Min(targetBody.velocity.magnitude * 0.05f, 2) + new Vector3(0, height, 0));
+                Vector3 localOffset = lookObj.TransformPoint(-lookDirActual * distance - lookDirActual * Mathf.Min(targetBody.velocity.magnitude * 0.05f, 2) + Vector3.up * height);
 
                 //Check if there is an object between the camera and target vehicle and move the camera in front of it
                 if (Physics.Linecast(target.position, localOffset, out hit, castMask))
